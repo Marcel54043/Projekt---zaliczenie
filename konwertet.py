9eb39f565b42408801f1ad90ab8cd968487c8da8
Marcel54043 <dsw54043@student.dsw.edu.pl>
2024-05-29 17:01:36 +0200

    zrobiono Task5

--- konwertet.py
+++ konwertet.py
@@ -4,12 +4,12 @@ import os
 import json
 import yaml
 
+
 def load_json(filename):
     with open(filename, 'r') as file:
         content = file.read()
         data = json.loads(content)
         print(f"Zawartość pliku JSON:\n{json.dumps(data, indent=4)}")
-        print("Plik JSON jest poprawny składniowo.")
         return data
 
 
@@ -17,18 +17,29 @@ def load_yaml(filename):
     with open(filename, 'r') as file:
         data = yaml.safe_load(file)
         print(f"Zawartość pliku YAML:\n{yaml.dump(data, default_flow_style=False)}")
-        print("Plik YAML jest poprawny składniowo.")
         return data
 
 
+def save_json(data, filename):
+    with open(filename, 'w') as output_file:
+        json.dump(data, output_file, indent=4)
+    print(f"Dane zostały zapisane do pliku {filename} w formacie JSON")
+
+
+def save_yaml(data, filename):
+    with open(filename, 'w') as output_file:
+        yaml.dump(data, output_file, default_flow_style=False)
+    print(f"Dane zostały zapisane do pliku {filename} w formacie YAML")
+
+
 def main():
-
     parser = argparse.ArgumentParser(
-        description='Prosty program do wyświetlania i zapisywania informacji o plikach JSON i YAML.')
+        description='konwerter')
 
-    
     parser.add_argument('filename', type=str, help='Ścieżka do pliku do przetworzenia')
     parser.add_argument('-o', '--output', type=str, help='Ścieżka do pliku wyjściowego')
+    parser.add_argument('-f', '--format', type=str, choices=['json', 'yaml'],
+                        help='Format pliku wyjściowego (json lub yaml)')
 
     args = parser.parse_args()
 
@@ -40,8 +51,10 @@ def main():
 
     print(f"Nazwa pliku: {os.path.basename(filename)}")
 
+
     mime_type, _ = mimetypes.guess_type(filename)
     if mime_type is None:
+
         if filename.endswith('.json'):
             mime_type = 'application/json'
         elif filename.endswith('.yaml') or filename.endswith('.yml'):
@@ -61,10 +74,15 @@ def main():
             return
 
         if args.output:
+            if not args.format:
+                print("Proszę podać format wyjściowy za pomocą opcji -f lub --format (json lub yaml).")
+                return
+
             try:
-                with open(args.output, 'w') as output_file:
-                    json.dump(data, output_file, indent=4)
-                print(f"Dane zostały zapisane do pliku {args.output}")
+                if args.format == 'json':
+                    save_json(data, args.output)
+                elif args.format == 'yaml':
+                    save_yaml(data, args.output)
             except Exception as e:
                 print(f"Nie udało się zapisać danych do pliku {args.output}: {e}")
     except (json.JSONDecodeError, yaml.YAMLError) as e:
