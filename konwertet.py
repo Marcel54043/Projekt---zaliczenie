f8c2a0a97889ad375d39e66ccccdbf8bbf0889a7
Marcel54043 <dsw54043@student.dsw.edu.pl>
2024-05-29 16:45:25 +0200

    zrobione Task2

--- konwertet.py
+++ konwertet.py
@@ -1,16 +1,17 @@
 import argparse
 import mimetypes
 import os
+import json
 
 
 def main():
-    
-    parser = argparse.ArgumentParser(description='Prosty program do wyświetlania informacji o pliku.')
-
-    
-    parser.add_argument('filename', type=str, help='Ścieżka do pliku do przetworzenia')
+  
+    parser = argparse.ArgumentParser(description='Prosty program do wyświetlania informacji o pliku JSON.')
 
    
+    parser.add_argument('filename', type=str, help='Ścieżka do pliku do przetworzenia')
+
+    
     args = parser.parse_args()
 
     
@@ -24,17 +25,27 @@ def main():
     
     print(f"Nazwa pliku: {os.path.basename(filename)}")
 
- 
+    
     mime_type, _ = mimetypes.guess_type(filename)
     if mime_type is None:
         mime_type = 'Nieznany'
     print(f"Rodzaj pliku: {mime_type}")
 
- 
+    
+    if mime_type != 'application/json':
+        print(f"Plik {filename} nie jest plikiem JSON.")
+        return
+
+    
     try:
         with open(filename, 'r') as file:
             content = file.read()
-            print(f"Zawartość pliku:\n{content}")
+            
+            data = json.loads(content)
+            print(f"Zawartość pliku JSON:\n{json.dumps(data, indent=4)}")
+            print("Plik JSON jest poprawny składniowo.")
+    except json.JSONDecodeError as e:
+        print(f"Nie udało się odczytać pliku {filename}: Błąd składni JSON - {e}")
     except Exception as e:
         print(f"Nie udało się odczytać pliku {filename}: {e}")
 
